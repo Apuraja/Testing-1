900ab3e2ab7631a06656cdfc41eeaca1ffaea5e2
Apuraja <apupatel17@gmail.com>
2022-12-24 10:58:38 +0000

    Five.cs is change

--- src/extra1/Five.java
+++ src/extra1/Five.java
@@ -2,7 +2,7 @@ package extra1;
 //           Write a java program to concat 2 strings.
 //            Expected output: I love java i eat java
 public class Five {
-    static String name = "I Love Java", name1 = " I eat java";
+    static String name = "I Love Java Simple language", name1 = " I eat java";
     public static void main(String[] args) {
         System.out.println(name.concat(name1));
     }
